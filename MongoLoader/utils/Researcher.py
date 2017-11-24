cb335a6c2faa1495094bc86aef7a9f956ff37ed7
Eric Jang <ericjang96@gmail.com>
2017-11-24 13:19:36 -0800

    Added random OrcID for testing

--- MongoLoader/utils/Researcher.py
+++ MongoLoader/utils/Researcher.py
@@ -1,14 +1,17 @@
 # Class definition for Researcher object
 class Researcher:
 
-    def __init__(self, name, department, institution):
+    def __init__(self, name, department, institution, orcid):
         self.name = name
         self.department = department
         self.institution = institution
+        # Adding the same random OrcID for testing purposes TODO: Fix this later
+        self.orcid = "0000-0003-0392-0702"
 
     def getDocument(self):
         return {
             'name' : self.name,
             'department' : self.department,
-            'institution' : self.institution
+            'institution' : self.institution,
+            'orcid' : self.orcid
         }
